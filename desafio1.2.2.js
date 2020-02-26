aa508a924a9c266d9b2e72a4b3ae5e257a42530b
BrunaMB <61436036+BrunaMB@users.noreply.github.com>
2020-02-26 22:52:30 +0000

    Desafios bootcamp

---
+++ desafio1.2.2.js
@@ -0,0 +1,16 @@
+const programador= [
+    {
+        nome:"C++",
+        especialidade:"Desktop",
+    },
+    {
+        nome:"Python",
+        especialidade:"Data Science",
+    },
+    {
+        nome:"JavaScript",
+        especialidade:"web/mobile",
+    }
+]
+
+console.log ( "O usuario Carlos tem 32 anos e usa " + programador[0].nome + " com especialidade em " + programador[0].especialidade);
