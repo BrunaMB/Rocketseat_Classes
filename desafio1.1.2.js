a0e7cfc32a3bfd11e7233abed4e15dc3b9d4dea0
BrunaMB <61436036+BrunaMB@users.noreply.github.com>
2020-02-26 22:35:47 +0000

    Desafios bootcamp

---
+++ desafio1.1.2.js
@@ -0,0 +1,18 @@
+const nome="Silvana";
+
+const sexo="F";
+
+const idade=48;
+    
+const contribuiçao=23;
+
+const idadeEContribuição= idade + contribuiçao;
+
+if (contribuiçao>=30 || idadeEContribuição>=85) {
+
+    console.log ("Parabéns " + nome + ", voce já pode se aposentar!");
+
+} else {
+
+    console.log ( nome + " Infelizmente voce ainda não pode se aposentar");
+}
---
+++ desafio1.2.js
@@ -0,0 +1,9 @@
+const empresa={
+    nome: "Rocketseat",
+    cor: "roxo",
+    foco: "programação",
+    endereço: "Rua Guilherme Gembala",
+    numero: 260
+}
+
+console.log("A empresa " + empresa.nome + " esta localizada em " + empresa.endereço + ", nº " + empresa.numero);
